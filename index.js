f16f9237ebf84307cb06baeaac18618a71b67019
AirLinkDev <thomas.trebacz@gmail.com>
2023-10-14 07:25:11 -0700

    Added loop to determine timestamp of latest data available from API

--- index.js
+++ index.js
@@ -23,31 +23,53 @@ app.use(express.static("public"));//This way any static files used in the ejs ca
 
 app.get("/", async(req, res) => {
     try {
-        const response = await axios.get(API_URL+API_getRUB+API_key);
+        let url = API_URL+API_getRUB+API_key;
+        console.log("my url "+url);
+        const response = await axios.get(url
+          );
         const temp = response.data["Time Series FX (Daily)"];
-        let dateObj = new Date();
-        let yms = dateObj.getTime()-(24*60*60*1000);
-        let ydateObj = new Date(yms);
-        let month = dateObj.getUTCMonth() + 1; //months from 1-12
-        let day = dateObj.getUTCDate();
-        let year = dateObj.getUTCFullYear();
-        let ymonth = ydateObj.getUTCMonth() + 1; //months from 1-12
-        let yday = ydateObj.getUTCDate();
-        let yyear = ydateObj.getUTCFullYear();
-        const today = year+"-"+month+"-"+day;
-        const yesterday = yyear+"-"+ymonth+"-"+yday;
+        //console.log("The response was "+JSON.stringify(temp));
+        let today;
+        let yesterday;
+        let tday;
+        let yeday;
         const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        //adjusts the timestamp backward by 12 hour increments(x) until data is acquired
+        for(let x=0;x<72;x++){
+          let newDate = new Date();
+          let dateObj = new Date(newDate.getTime()-x*12*60*60*1000);
+          let yms = dateObj.getTime()-(24*60*60*1000);
+          let ydateObj = new Date(yms);
+          let month = dateObj.getUTCMonth() + 1; //months from 1-12
+          let day = dateObj.getUTCDate();
+          let year = dateObj.getUTCFullYear();
+          let ymonth = ydateObj.getUTCMonth() + 1; //months from 1-12
+          let yday = ydateObj.getUTCDate();
+          let yyear = ydateObj.getUTCFullYear();
+          today = year+"-"+month+"-"+day;
+          yesterday = yyear+"-"+ymonth+"-"+yday;
+          
+          tday = weekday[dateObj.getDay()];
+          yeday = weekday[ydateObj.getDay()];
+          console.log("Now testing today: "+today);
+          console.log("yesterday "+yesterday);
+          if(temp[today]===undefined){
+            console.log(today+" is undefined");          
+          }else{
+            
+            break;
+          }
+        }
+          console.log("Acquired fresh data from: "+today+" with value: "+JSON.stringify(temp[today]));
+          const rublToday = temp[today];
+          const rublYesterday = temp[yesterday];
 
         
-        let tday = weekday[dateObj.getDay()];
-        let yeday = weekday[ydateObj.getDay()];
-        const rublToday = temp[today];
-        const rublYesterday = temp[yesterday];
-        let diff = "The Ruble shrank in value by: "+((rublToday["2. high"]-rublYesterday["2. high"])/rublToday["2. high"]).toFixed(4)*100+"% (USD)"
+        let diff = "The Ruble shrank in value by: "+((rublToday["2. high"]-rublYesterday["2. high"])/rublToday["2. high"]).toFixed(4)*100+"% against USD"
         let state = "sadPutin";
         if((rublToday["2. high"]<rublYesterday["2. high"] )&&(rublToday["2. high"]<100)){
           state = "happyPutin";
-          diff = "The Ruble grew in value by: "+((rublYesterday["2. high"]-rublToday["2. high"])/rublYesterday["2. high"]).toFixed(4)*100+"% (USD)"
+          diff = "The Ruble grew in value by: "+((rublYesterday["2. high"]-rublToday["2. high"])/rublYesterday["2. high"]).toFixed(4)*100+"% against USD"
         }
         console.log("Rubl for " +today+": "+JSON.stringify(rublToday));
         console.log("Rubl for " +yesterday+": "+JSON.stringify(rublYesterday));
